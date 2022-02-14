180539dcf85e1e82009869daeb25b607bac39b06
Brightology1 <bright.anokwuru@decagon.dev>
2022-02-14 11:52:36 +0100

    Updated test 2

---
+++ src/test/java/com/brightology/jwtdemo/SecurityConfigurerTest.java
@@ -0,0 +1,31 @@
+package com.brightology.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+
+class SecurityConfigurerTest {
+    @Test
+    void testPasswordEncoder() {
+        // TODO: This test is incomplete.
+        //   Reason: R002 Missing observers.
+        //   Diffblue Cover was unable to create an assertion.
+        //   Add getters for the following fields or make them package-private:
+        //     SecurityConfigurer.jwtRequestFilter
+        //     SecurityConfigurer.myUserDetailsService
+        //     WebSecurityConfigurerAdapter.authenticationBuilder
+        //     WebSecurityConfigurerAdapter.authenticationConfiguration
+        //     WebSecurityConfigurerAdapter.authenticationManager
+        //     WebSecurityConfigurerAdapter.authenticationManagerInitialized
+        //     WebSecurityConfigurerAdapter.contentNegotiationStrategy
+        //     WebSecurityConfigurerAdapter.context
+        //     WebSecurityConfigurerAdapter.disableDefaults
+        //     WebSecurityConfigurerAdapter.disableLocalConfigureAuthenticationBldr
+        //     WebSecurityConfigurerAdapter.http
+        //     WebSecurityConfigurerAdapter.localConfigureAuthenticationBldr
+        //     WebSecurityConfigurerAdapter.logger
+        //     WebSecurityConfigurerAdapter.objectPostProcessor
+        //     WebSecurityConfigurerAdapter.trustResolver
+
+        (new SecurityConfigurer()).passwordEncoder();
+    }
+}
+
